e1be10c61a06ebfa4561eb5f5c1a0fa994372f9b
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-12-04 01:36:52 -0300

    cadastro

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import styles from './page.module.css'
+import Professional from './ui/forms/professional'
 
 export default function Home() {
-  return <main className={styles.main}>MVP</main>
+  return (
+    <main className={styles.main}>
+      <Professional title="Cadastro" />
+    </main>
+  )
 }
