a596f813ad7a3824c5987d95b13dcc5be02bf9d2
Orivaldo Santana Jr <orivaldo@gmail.com>
2024-05-14 00:33:06 -0300

    Draft

--- app/lib/user/action.ts
+++ app/lib/user/action.ts
@@ -6,4 +6,25 @@ export const addUser = async (formData: FormData) => {
   let cellphone = formData.get('cellphone')
 
   console.log(`addUser ${name} ${email} ${cellphone}`)
+  /*
+  export async function getServerSideProps(context) {
+  const prisma = new PrismaClient()
+  let user
+
+  try {
+    await prisma.user.create({
+      data: {
+        name: name, 
+        email: email,
+        cellphone: cellphone,
+        knowledge: 'none',
+        profile: 'none', 
+      },
+    })
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await prisma.$disconnect()
+  }
+  */
 }
