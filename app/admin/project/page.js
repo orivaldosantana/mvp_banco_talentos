85c9e9bb785d981eb685cc6ad7fc035e12580b4d
Orivaldo Santana Jr <orivaldo@gmail.com>
2024-05-15 18:10:49 -0300

    close #15

--- app/admin/project/page.js
+++ app/admin/project/page.js
@@ -1,8 +1,9 @@
 // https://github.com/vercel/next.js/blob/canary/examples/next-forms/app/actions.ts
+import Page from '../../ui/page'
 export default function ProjectPage() {
   return (
     <div>
-      <h1> Projetos </h1>
+      <Page title={'Projetos'}>xxxx</Page>
     </div>
   )
 }
