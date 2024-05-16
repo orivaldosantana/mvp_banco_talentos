6469d3babce38e6681513c503fcdcea2f09d6506
Orivaldo Santana Jr <orivaldo@gmail.com>
2024-05-15 22:13:46 -0300

    up

--- app/api/project/route.ts
+++ app/api/project/route.ts
@@ -2,27 +2,6 @@ export const POST = async (req: Request) => {
   const requestData = await req.json()
   console.log(requestData)
   return Response.json({
-    message: 'Cadastro realizado com sucesso!'
+    message: 'Teste de API realizado com sucesso!'
   })
-  /*
-  const { name, description, status, startDate, endDate, userId } = req.body
-  const prisma = new PrismaClient()
-  try {
-    const project = await prisma.project.create({
-      data: {
-        name,
-        description,
-        status,
-        startDate: new Date(startDate),
-        endDate: new Date(endDate),
-        user: { connect: { id: userId } }
-      }
-    })
-    res.status(200).json(project)
-  } catch (error) {
-    res.status(500).json({ message: 'Erro ao realizar o cadastro!', type: 'error' })
-  } finally {
-    await prisma.$disconnect()
-  }
-  */
 }
