68ae57303edd253a5931ebf0fddc2da9ee553230
Orivaldo Santana <orivaldo@gmail.com>
2024-12-24 20:22:59 -0300

    Redirects unauthenticated users to the home page in the NavBar

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -11,9 +11,16 @@ import styles from '../page.module.css'
 import { handleGitHubLogout } from '../lib/action'
 import { auth } from '../lib/auth'
 
+import { redirect } from 'next/navigation'
+
 const NavBar = async () => {
   const session = await auth()
+
   console.log(session)
+  if (!session) {
+    redirect('/')
+  }
+
   return (
     <div className={styles.menu}>
       <nav>
